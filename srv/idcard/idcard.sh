76070915b41b61d64694d34f584e94f4d52e7fcd
Keunotorcagoule <roxanne.roulland@ynov.com>
2021-11-22 21:41:56 +0100

    modif idcard

--- srv/idcard/idcard.sh
+++ srv/idcard/idcard.sh
@@ -5,5 +5,4 @@ echo -e "Ram : $(bc <<< "scale=2; $(cat /proc/meminfo | grep MemAvailable | awk 
 echo -e "Disque : $(df -Pk -BG / | sed 1d | awk '{ print $4 }') space left\n"
 echo -e "Top 5 processes by RAM usage :\n$(ps aux --sort=-%mem | head -5 | tail -4 | awk '{ print $11 }') \n$(ps aux --sort=-%mem | head -6 | tail -1 | awk '{ print $12 }')\n"
 echo -e "Listening ports: \n$(sudo lsof -i -P -n | grep LISTEN | awk '{ print $2 " : " $1 }')\n"
-echo "Here's your random cat : $(curl https://api.thecatapi.com/v1/images/search)"
-
+echo "Here's your random cat : "$(curl -s https://api.thecatapi.com/v1/images/search | grep '"url"' | cut -d'"' -f10)""
